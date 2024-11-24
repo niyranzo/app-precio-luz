158254b6698ee3f57d7b0a1a162b485853cc215d
niyranzo <yranzo.nicole@gmail.com>
2024-11-24 19:27:19 +0100

    Creado los endpoints

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,19 @@
 import express from 'express';
-import productRoutes from "./routes/products.js";
+import dayRouter from "./routers/dayRouter.js";
+import hoursRouter from "./routers/hoursRouter.js";
+import { PORT } from './config/config.js';
 
-// creamos la instancia de express
+// Crear la instancia de express
 const app = express();
-// usamos el middleware (funciones q se ecuentran entre el usuario y la api) para que nuestro servidor pueda entender json
+
+// Usar middleware para que el servidor pueda entender JSON
 app.use(express.json());
-// rutas que podemos usar
-app.use("/api/products", productRoutes);
-// puerto en el que se va a correr el servidor 
-const PORT = 3000;
+
+// Rutas que podemos usar
+app.use("/api/day", dayRouter);
+app.use("/api/hour", hoursRouter);
+
+// Puerto en el que se va a correr el servidor 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
