e44ea442da7242762efed8ddcd1f7871b0a36e65
sariasss <sara.arias085@gmail.com>
2024-11-25 18:21:06 +0100

    terminated main, nav, footer y style.css

--- fronted/src/main.js
+++ fronted/src/main.js
@@ -1,25 +1,30 @@
 //importaciones
 import { createThemeButton, darkButton, lightButton } from "./components/botonTema/botonTema";
+import { createFooter } from "./components/footer/footer";
+import { createMain } from "./components/main/main";
+import { createNav } from "./components/nav/nav";
 import { createSpinner, showSpinner, hideSpinner } from "./components/spinner/spinner";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const app = document.getElementById("app"); // div #app del index
-    const nav = document.createElement("nav");
+    const app = document.getElementById("app");
+  
+    const nav = createNav();
+    const main = createMain();
+    const footer = createFooter();
     const themeBtn = createThemeButton();
-    nav.classList.add("nav"); 
-    /*
     // Crear y añadir el spinner
     const spinner = createSpinner();
-    app.appendChild(spinner);
-    hideSpinner();
-    showSpinner();
-    */
+    
+    nav.appendChild(themeBtn);
+    main.appendChild(spinner);
 
     // Crear y añadir el botón de tema
-    
-    app.appendChild(themeBtn);
-    nav.appendChild(themeBtn); 
     app.appendChild(nav);
+    app.appendChild(main);
+    app.appendChild(footer);
+
+    hideSpinner();
+    showSpinner();
 
     // Cambio de tema al hacer clic
     const tema = () => {
@@ -31,5 +36,5 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     themeBtn.addEventListener("click", tema);
-
+    console.log("adf");
 });
