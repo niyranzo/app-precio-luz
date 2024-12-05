964feeaa27c81105382a57a120a508fd43391d7e
sariasss <sara.arias085@gmail.com>
2024-12-05 18:50:28 +0100

    detalles

--- fronted/src/components/grafico/grafico.js
+++ fronted/src/components/grafico/grafico.js
@@ -12,14 +12,14 @@ export const chart = (data) =>{
     });
     const canva = document.getElementById('miCanva');
     return new Chart(canva, {
-      type: 'bar', // Tipo de gráfica: 'bar', 'line', 'pie', etc.
+      type: 'line', // Tipo de gráfica: 'bar', 'line', 'pie', etc.
       data: {
         labels: horas, // Etiquetas en el eje X
         datasets: [{
           label: 'Precio',
           data: precios, // Datos de la gráfica
-          backgroundColor: "rgba(182, 168, 199, 0.422)",
-          borderColor:  "rgba(182, 168, 199, 1)",
+          backgroundColor: "#ffabc6",
+          borderColor:  "#ffabc6",
           borderWidth: 1
         }]
       },
