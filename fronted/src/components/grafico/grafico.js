c92ee9643fce2e02f325f1785fbcab166b12fe3a
sariasss <sara.arias085@gmail.com>
2024-12-05 18:24:01 +0100

    detalles botones, nav y footer

---
+++ fronted/src/components/grafico/grafico.js
@@ -0,0 +1,35 @@
+import './chart.css'
+export const chart = (data) =>{
+    const horas = [];
+    const precios = [];
+    data.keys().forEach(k => {
+        const datos=data.get(k);
+        datos.forEach(({ precio, hora })=>{
+            horas.push(hora);
+            precios.push(precio);
+            ;
+        })
+    });
+    const canva = document.getElementById('miCanva');
+    return new Chart(canva, {
+      type: 'bar', // Tipo de gráfica: 'bar', 'line', 'pie', etc.
+      data: {
+        labels: horas, // Etiquetas en el eje X
+        datasets: [{
+          label: 'Precio',
+          data: precios, // Datos de la gráfica
+          backgroundColor: "rgba(182, 168, 199, 0.422)",
+          borderColor:  "rgba(182, 168, 199, 1)",
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+}
